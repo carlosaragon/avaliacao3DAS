796cf224e27c4bda94028be22ca9fb27c480bd44
Carlos Aragon <carlos.aragon@ipea.gov.br>
2018-10-24 17:03:09 -0300

    Questao 1: Carlos Aragon - 15/0121148

---
+++ Cadastros/DuracaoMinutos.java
@@ -0,0 +1,23 @@
+package Cadastros;
+
+import Cadastros.Viagem;
+
+public class DuracaoMinutos {
+	private final Viagem _viagem;
+	private int duracaoMinutos;
+	
+	public DuracaoMinutos(Viagem source) {
+		this._viagem = source;
+	}
+	
+	public int compute() {
+		if (_viagem.getMinutosTermino() > _viagem.getMinutoInicio()) 
+			duracaoMinutos = _viagem.getMinutosTermino() - _viagem.getMinutoInicio();
+		else {
+			duracaoMinutos = 60 - _viagem.getMinutoInicio() + _viagem.getMinutosTermino();
+			if (duracaoMinutos == 60) //caso especial
+				duracaoMinutos = 0;
+		}
+		return duracaoMinutos;
+	}
+}
